414a33695623d90c40dcb72fadb5a95915f1e6d6
Hiro Ito <mobiiii24@gmail.com>
2026-05-04 12:40:32 +0900

    publishing test

--- src/fast_dcp/__init__.py
+++ src/fast_dcp/__init__.py
@@ -1,6 +1,6 @@
 """A CLI tool that provides shorthand aliases for common docker compose commands."""
 
-__version__ = "0.0.2"
+__version__ = "0.0.2.dev3"
 
 from fast_dcp.main import main, dcpu_main, dcpe_main
 
