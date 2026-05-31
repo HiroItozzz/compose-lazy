97f19ee8aeffd602f436456f4809003cb82c3153
Hiro Ito <mobiiii24@gmail.com>
2026-05-31 14:51:35 +0900

    refac: add allow_zero argument to interactive select
    - add overload, docstring

--- src/fast_dcp/cli_utils.py
+++ src/fast_dcp/cli_utils.py
@@ -1,15 +1,48 @@
 import logging
 import sys
-from typing import Iterable
+from typing import Iterable, Literal, overload
 
 logger = logging.getLogger(__name__)
 
 
+@overload
 def interactive_select(
-    choice_list: Iterable[str], flag: str | None = None, multiple: bool = True
-) -> list[str]:
+    choice_list, flag=..., *, multiple=..., allow_zero: Literal[True]
+) -> None | list[str]: ...
+@overload
+def interactive_select(
+    choice_list, flag=..., *, multiple=..., allow_zero: Literal[False] = ...
+) -> list[str]: ...
 
-    choice_list = sorted(choice_list)
+
+def interactive_select(
+    candidates: Iterable[str],
+    flag: str | None = None,
+    *,
+    multiple: bool = True,
+    allow_zero: bool = False,
+) -> list[str] | None:
+    """Starts general interactive session.
+
+    When `allow_zero` is False, returns list[str].
+    Only when `allow_zero` is True, may return None (if the user inputs exactly `0`).
+    Despite `multiple` being True or False, the choice `0` cannot be included in multiple choices (the session continues).
+
+    Args:
+        candidates (Iterable[str]): list, set, dict, etc... and generator-like objects.
+        flag (str | None, optional): Specified when each candidate needs prefix tag in list. Defaults to None.
+        multiple (bool, optional): Enables multiple select. Defaults to True.
+        allow_zero (bool, optional): Enables the choice `0` make it return None. Defaults to False.
+
+    Raises:
+        SystemExit: Raised when user input `q` or `Q`.
+        KeyboardInterrupt: Raised when user stopped session by Ctrl+C.
+
+    Returns:
+        list[str] | None: The list of candidate name(s) user selected, or None when `0` inputed.
+    """
+
+    candidates = sorted(candidates)
     args = []
     prompt = (
         "\nEnter your choices (e.g., 1,3,4) or 'q' to quit: "
@@ -23,8 +56,8 @@ def interactive_select(
     )
 
     # Show choices
-    for idx, choice in enumerate(choice_list, start=1):
-        print(f"{idx:>5}. {choice}")
+    for idx, candidate in enumerate(candidates, start=1):
+        print(f"{idx:>5}. {candidate}")
 
     # User input
     while True:
@@ -39,13 +72,18 @@ def interactive_select(
                     (i.strip() for i in choices_str.split(",") if i.strip()),
                 )
             )
+            if allow_zero:
+                if choices == [-1]:  # When input is "0"
+                    return None
+                if len(choices) > 1 and -1 in choices:  # When multiple input includes "0"
+                    raise ValueError
             if not multiple and len(choices) != 1:
                 raise ValueError
             if any((i < 0 for i in choices)):
                 raise IndexError
 
             for idx in choices:
-                chosen = choice_list[idx]
+                chosen = candidates[idx]
                 if flag is None:
                     args += [chosen]
                 else:
